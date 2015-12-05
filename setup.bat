213a76c5111d9d3f4ab3c2d7d742a71612d1cec2
Dax T. Games <dtgames@kinggeek.org>
2015-12-05 08:50:24 -0500

    additionl cmder integration

--- setup.bat
+++ setup.bat
@@ -2,78 +2,93 @@
 
 setlocal enabledelayedexpansion
 
-call :do_isAdmin
 
-if "%is_admin%" == "1" (
-  if exist "%USERPROFILE%\.vimrc" (
-    echo Backing up "%USERPROFILE%\.vimrc" to ".vimrc.yadr4win
-    ren "%USERPROFILE%\.vimrc" ".vimrc.yadr4win"
-  )
+if exist "%USERPROFILE%\.yadr4win" (
+  echo "YADR4WIN is already installed, nothing was changed!
+) else (
+  call :do_isAdmin
 
-  mklink /h "%USERPROFILE%\.vimrc" "%USERPROFILE%\.yadr4win\vimrc"
-
-  if exist "%USERPROFILE%\.gitconfig" (
-    echo Backing up "%USERPROFILE%\.gitconfig" to ".gitconfig.yadr4win
-    ren "%USERPROFILE%\.gitconfig" ".gitconfig.yadr4win"
-  )
-
-  mklink /h "%USERPROFILE%\.gitconfig" "%USERPROFILE%\.yadr4win\git\gitconfig"
-
-  if exist "%USERPROFILE%\.tmux.conf" (
-    echo Backing up "%USERPROFILE%\.tmux.conf" to ".tmux.conf.yadr4win
-    ren "%USERPROFILE%\.tmux.conf" ".tmux.conf.yadr4win"
-  )
-
-  mklink /h "%USERPROFILE%\.tmux.conf" "%USERPROFILE%\.yadr4win\tmux\tmux.conf"
-
-  if exist "%USERPROFILE%\.vim" (
-    dir %USERPROFILE%\.vim | findstr /i "%USERPROFILE%\.yadr4win\vim" >NULL
-    if "!ERRORLEVEL!" == "1" (
-      echo Backing up "%USERPROFILE%\.vim" to ".vim.yadr4win
-      rendir "%USERPROFILE%\.vim" ".vim.yad4rwin"
+  if "%is_admin%" == "1" (
+    if exist "%USERPROFILE%\.vimrc" (
+      echo Backing up "%USERPROFILE%\.vimrc" to ".vimrc.yadr4win
+      ren "%USERPROFILE%\.vimrc" ".vimrc.yadr4win"
     )
-  )
 
-  if not exist "%USERPROFILE%\.vim" (
-    mklink /d "%USERPROFILE%\.vim" "%USERPROFILE%\.yadr4win\vim"
-  )
+    mklink /h "%USERPROFILE%\.vimrc" "%USERPROFILE%\.yadr4win\vimrc"
 
-  if not exist "%USERPROFILE%\.vim\bundle" (
-    mkdir "%USERPROFILE%\.vim\bundle"
-  )
+    if exist "%USERPROFILE%\.gitconfig" (
+      echo Backing up "%USERPROFILE%\.gitconfig" to ".gitconfig.yadr4win
+      ren "%USERPROFILE%\.gitconfig" ".gitconfig.yadr4win"
+    )
 
-  if not exist "%USERPROFILE%\.vim\bundle\vundle.vim" (
-    echo Installing 'vundle.vim'...
-    git clone https://github.com/VundleVim/Vundle.vim.git "%USERPROFILE%\.vim\bundle\vundle.vim"
-  )
+    mklink /h "%USERPROFILE%\.gitconfig" "%USERPROFILE%\.yadr4win\git\gitconfig"
 
-  if exist "%USERPROFILE%\.aliases.doskey" (
-    "%USERPROFILE%\.aliases.doskey" "%USERPROFILE%\aliases.doskey.bak"
-  )
+    if exist "%USERPROFILE%\.tmux.conf" (
+      echo Backing up "%USERPROFILE%\.tmux.conf" to ".tmux.conf.yadr4win
+      ren "%USERPROFILE%\.tmux.conf" ".tmux.conf.yadr4win"
+    )
 
-  mklink /H "%USERPROFILE%\.aliases.doskey" "%USERPROFILE%\.yadr4win\aliases.doskey"
+    mklink /h "%USERPROFILE%\.tmux.conf" "%USERPROFILE%\.yadr4win\tmux\tmux.conf"
 
-  if exist "%USERPROFILE%\.tmux.conf" (
-    "%USERPROFILE%\.aliases.sh" "%USERPROFILE%\aliases.sh.bak"
-  )
-   
-  mklink /H "%USERPROFILE%\.aliases.sh" "%USERPROFILE%\.yadr4win\aliases.sh"
+    if exist "%USERPROFILE%\.vim" (
+      dir %USERPROFILE%\.vim | findstr /i "%USERPROFILE%\.yadr4win\vim" >NULL
+      if "!ERRORLEVEL!" == "1" (
+        echo Backing up "%USERPROFILE%\.vim" to ".vim.yadr4win
+        rendir "%USERPROFILE%\.vim" ".vim.yad4rwin"
+      )
+    )
 
-  set do_gitconfig.user=0
-  if not exist "%USERPROFILE%\.gitconfig.user" (
-    set do_gitconfig.user=8
-  ) else (
-    type "%USERPROFILE%\.gitconfig.user" | findstr /i /R /C:"^\[user\]$">nul
-    if !ERRORLEVEL! GTR 0 ( set /a do_gitconfig.user+=4 )
-    type "%USERPROFILE%\.gitconfig.user" | findstr /i /R /C:"^ *name *= *[a-z]*.*$">nul
-    if !ERRORLEVEL! GTR 0 ( set /a do_gitconfig.user+=2 )
-    type "%USERPROFILE%\.gitconfig.user" | findstr /i /r /c:"^ *email *= .*@.*$">nul
-    if !ERRORLEVEL! GTR 0 ( set /a do_gitconfig.user+=1 )
+    if not exist "%USERPROFILE%\.vim" (
+      mklink /d "%USERPROFILE%\.vim" "%USERPROFILE%\.yadr4win\vim"
+    )
 
-    if "%do_gitconfig.user%" == "8" (
-      call :do_gitconfig.user
-    ) else if "%do_gitconfig.user%" GEQ "1" if "%do_gitconfig.user%" LEQ "7" (
-      call :do_gitconfig.user
+    if not exist "%USERPROFILE%\.vim\bundle" (
+      mkdir "%USERPROFILE%\.vim\bundle"
+    )
+
+    if not exist "%USERPROFILE%\.vim\bundle\vundle.vim" (
+      echo Installing 'vundle.vim'...
+      git clone https://github.com/VundleVim/Vundle.vim.git "%USERPROFILE%\.vim\bundle\vundle.vim"
+    )
+
+    if defined CMDER_DEV (
+      set ALIASES_PATH=%CMDER_ROOT%\config\aliases
+      set ALIASES__SH_PATH=%CMDER_ROOT%\config\user-aliases.sh
+    ) else (
+      set ALIASES_PATH=%USER_PROFILE%\.aliases
+      set ALIASES_SH_PATH=%USER_PROFILE%\.aliases.sh
+    )
+
+    if exist "%ALIASES_PATH%" (
+      echo Backing up "%ALIASES_PATH%" to "%ALIASES_PATH%.yadr4win"
+      move "%ALIASES_PATH%" "%ALIASES_PATH%.yadr4win"
+    )
+
+    mklink /H "%ALIASES_PATH%" "%USERPROFILE%\.yadr4win\aliases.doskey"
+
+    if exist "%ALIASES_SH_PATH%" (
+      echo Backing up "%ALIASES_SH_PATH%" to "%ALIASES_SH_PATH%.yadr4win"
+      move "%ALIASES_SH_PATH%" "%ALIASES_SH_PATH%.yadr4win"
+    )
+
+    mklink /H "%ALIASES_SH_PATH%" "%USERPROFILE%\.yadr4win\aliases.sh"
+
+    set do_gitconfig.user=0
+    if not exist "%USERPROFILE%\.gitconfig.user" (
+      set do_gitconfig.user=8
+    ) else (
+      type "%USERPROFILE%\.gitconfig.user" | findstr /i /R /C:"^\[user\]$">nul
+      if !ERRORLEVEL! GTR 0 ( set /a do_gitconfig.user+=4 )
+      type "%USERPROFILE%\.gitconfig.user" | findstr /i /R /C:"^ *name *= *[a-z]*.*$">nul
+      if !ERRORLEVEL! GTR 0 ( set /a do_gitconfig.user+=2 )
+      type "%USERPROFILE%\.gitconfig.user" | findstr /i /r /c:"^ *email *= .*@.*$">nul
+      if !ERRORLEVEL! GTR 0 ( set /a do_gitconfig.user+=1 )
+
+      if "%do_gitconfig.user%" == "8" (
+        call :do_gitconfig.user
+      ) else if "%do_gitconfig.user%" GEQ "1" if "%do_gitconfig.user%" LEQ "7" (
+        call :do_gitconfig.user
+      )
     )
   )
 )

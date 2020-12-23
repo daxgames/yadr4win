bd27703c9426cbe56d941293aab073d6a4520530
dgames <dgames@dtg.local>
2020-12-23 07:04:32 -0500

    'updates

--- user_aliases.ps1
+++ user_aliases.ps1
@@ -1,11 +1,15 @@
 new-alias ssh $env:cmder_root\vendor\git-for-windows\usr\bin\ssh.exe -erroraction silentlycontinue
 
 function va {
-  vi "$ENV:CMDER_ROOT\config\user-aliases.ps1"
+  vi "$ENV:CMDER_ROOT\config\user_aliases.ps1"
 }
 
 function gs {
   git status
+}
+
+function gi(){
+  vi .gitignore
 }
 
 function ga($git_args) {
@@ -16,6 +20,12 @@ function ga($git_args) {
   }
 
   git add $git_args
+}
+
+function gc($git_args) {
+  $git_args = $args -join ' '
+
+  git commit $git_args
 }
 
 new-alias sublime_text '$env:cmder_root\vendor\sublime_text_3\sublime_text' -erroraction silentlycontinue
